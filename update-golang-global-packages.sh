32c6cdedb9b1d44f97fb6223eeb0916e1fb3f79f
Daniel Mesquita <60039311+danielmesquitta@users.noreply.github.com>
2024-10-29 09:44:33 -0300

    Update update-golang-global-packages.sh

--- update-golang-global-packages.sh
+++ update-golang-global-packages.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 packages=(
+    "golang.org/x/tools/gopls@latest"
+    "github.com/cweill/gotests/gotests@latest"
+    "github.com/fatih/gomodifytags@latest"
+    "github.com/josharian/impl@latest"
+    "github.com/haya14busa/goplay/cmd/goplay@latest"
+    "github.com/go-delve/delve/cmd/dlv@latest"
+    "github.com/golangci/golangci-lint/cmd/golangci-lint@latest"
     "go.uber.org/nilaway/cmd/nilaway@latest"
     "github.com/air-verse/air@latest"
-    "github.com/golangci/golangci-lint/cmd/golangci-lint@latest"
     "github.com/segmentio/golines@latest"
     "github.com/sqlc-dev/sqlc/cmd/sqlc@latest"
     "github.com/swaggo/swag/cmd/swag@latest"
@@ -17,4 +23,3 @@ for package in "${packages[@]}"; do
 done
 
 echo "All packages have been updated successfully."
-
